c46a8adaf18ad5a436777f13c9698dec32ba8567
Shawn Drape <shawn@dra.pe>
2023-12-02 01:18:21 -0500

    day 1 TDD red with hint

--- 2023/day1/day1.dart
+++ 2023/day1/day1.dart
@@ -4,78 +4,79 @@ import 'dart:io';
 import 'package:test/test.dart';
 
 void main() {
-  group('Trebuchet - part 1', () {
-    /*
---- Day 1: Trebuchet?! ---
-Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
+//   group('Trebuchet - part 1', () {
+//     /*
+// --- Day 1: Trebuchet?! ---
+// Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 
-You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
+// You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
 
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+// Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
 
-You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
+// You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
 
-As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
+// As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
 
-The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+// The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 
-For example:
+// For example:
 
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
+// 1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet
 
-In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+// In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
-Consider your entire calibration document. What is the sum of all of the calibration values?
-*/
-    int calibrate(String value) {
-      final stripped_numbers = value.replaceAll(RegExp(r'[a-z]'), '');
-      final List<int> digits =
-          stripped_numbers.split('').map(int.parse).toList();
-      if (digits.length < 1) {
-        throw Exception('No number found');
-      }
-      if (digits.length == 1) {
-        return int.parse("${digits.first}${digits.first}");
-      }
-      return int.parse("${digits.first}${digits.last}");
-    }
+// Consider your entire calibration document. What is the sum of all of the calibration values?
+// */
+//     int calibrate(String value) {
+//       final stripped_numbers = value.replaceAll(RegExp(r'[a-z]'), '');
+//       final List<int> digits =
+//           stripped_numbers.split('').map(int.parse).toList();
+//       if (digits.length < 1) {
+//         throw Exception('No number found');
+//       }
+//       if (digits.length == 1) {
+//         return int.parse("${digits.first}${digits.first}");
+//       }
+//       return int.parse("${digits.first}${digits.last}");
+//     }
 
-    test('calibration passed for example values', () {
-      var example_inputs = <String>[
-        '1abc2',
-        'pqr3stu8vwx',
-        'a1b2c3d4e5f',
-        'treb7uchet'
-      ];
+//     test('calibration passed for example values', () {
+//       var example_inputs = <String>[
+//         '1abc2',
+//         'pqr3stu8vwx',
+//         'a1b2c3d4e5f',
+//         'treb7uchet'
+//       ];
 
-      var target_values = [12, 38, 15, 77];
+//       var target_values = [12, 38, 15, 77];
 
-      var calibrated_values = example_inputs.map(calibrate);
+//       var calibrated_values = example_inputs.map(calibrate);
 
-      expect(calibrated_values, equals(target_values));
-    });
+//       expect(calibrated_values, equals(target_values));
+//     });
 
-    test('calibration passes for input file', () async {
-      final expected_calibration_sum = 55172;
+//     test('calibration passes for input file', () async {
+//       final expected_calibration_sum = 55172;
 
-      final input = File('day1/day1_input.txt');
-      final lines =
-          input.openRead().transform(utf8.decoder).transform(LineSplitter());
-      var calibration_sum = 0;
-      try {
-        await for (var line in lines) {
-          var calibrated_value = calibrate(line);
-          calibration_sum += calibrated_value;
-        }
-      } catch (e) {
-        print('oops: $e');
-      }
-      expect(calibration_sum, equals(expected_calibration_sum));
-    });
-  });
+//       final input = File('day1/day1_input.txt');
+//       final lines =
+//           input.openRead().transform(utf8.decoder).transform(LineSplitter());
+//       var calibration_sum = 0;
+//       try {
+//         await for (var line in lines) {
+//           var calibrated_value = calibrate(line);
+//           calibration_sum += calibrated_value;
+//         }
+//       } catch (e) {
+//         print('oops: $e');
+//       }
+//       expect(calibration_sum, equals(expected_calibration_sum));
+//     });
+//   });
+
   group('Trebuchet - part 2', () {
     /* 
     --- Part Two ---
@@ -129,7 +130,6 @@ In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Addi
 
     int calibrate(String value) {
       final digitized_value = convert_digits(value);
-      print([value, digitized_value]);
       final stripped_numbers = digitized_value.replaceAll(RegExp(r'[a-z]'), '');
       final List<int> digits =
           stripped_numbers.split('').map(int.parse).toList();
@@ -142,7 +142,7 @@ In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Addi
       return int.parse("${digits.first}${digits.last}");
     }
 
-    test('example', () {
+    test('calibration passed for example values', () {
       final example_inputs = [
         'two1nine',
         'eightwothree',
@@ -158,6 +158,71 @@ In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Addi
       var calibrated_values = example_inputs.map(calibrate);
 
       expect(calibrated_values, equals(expected_calibrations));
+      expect(calibrated_values.reduce((value, element) => value + element),
+          equals(281));
+    });
+
+    test('hint from reddit thread', () {
+      expect(calibrate('eighthree'), equals(83));
+    }, solo: true);
+
+    test('additional samples from test file', () {
+      var values = [
+        'fivefourcxdfgbtnhmscdlsrnnljvgjlthree1f',
+        '4hsgqrzthtk',
+        '3ninez24',
+        'k9qgqsbbqdsj44tr',
+        'djcsxdmd6jrgtmxpk5onegrljnflbhmnnx',
+        'sxxrhpkpclbjms3jq',
+        '8ngmpfiveoneninerpbscdgjxztkzldcjbrk',
+        '637zbrfpsvhj2hzmvx',
+        '4eight6chc6',
+        '5one9nine',
+        '7sevengqrqztfvmhkqnjveightl2twovtgdlhv',
+        'twosixeighteightmm62',
+        'zlbtcjgninevbctrmsqkqkkjmvcfthnfvnl4',
+        'fivesjmgr7two',
+      ];
+
+      var expected_calibrations = [
+        51,
+        44,
+        34,
+        94,
+        61,
+        33,
+        89,
+        62,
+        46,
+        59,
+        72,
+        22,
+        94,
+        52
+      ];
+
+      expect(expected_calibrations, values.map(calibrate));
+    });
+
+    test('calibration passes for input file', () async {
+      final expected_calibration_sum = 0;
+
+      final input = File('day1/day1_input.txt');
+      final lines =
+          input.openRead().transform(utf8.decoder).transform(LineSplitter());
+      var calibration_sum = 0;
+      try {
+        await for (var line in lines) {
+          var calibrated_value = calibrate(line);
+          assert(calibrated_value < 100);
+          calibration_sum += calibrated_value;
+        }
+      } catch (e) {
+        print('oops: $e');
+      }
+
+      expect(calibration_sum, lessThan(54953));
+      expect(calibration_sum, equals(expected_calibration_sum));
     });
   });
 }
