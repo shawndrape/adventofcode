a678de406f7e82e40919bc9831301c90638bfa79
Shawn Drape <shawn@dra.pe>
2023-12-02 14:40:22 -0500

    day2 factory parser

--- 2023/day2/day2.dart
+++ 2023/day2/day2.dart
@@ -1,23 +1,60 @@
-import 'package:test/test.dart';
+import 'package:spec/spec.dart';
 
 class Game {
   int id;
-  List<({int red, int green, int blue})> matches;
+  Iterable<({int red, int green, int blue})> matches;
 
   Game(this.id, this.matches);
 
   factory Game.fromLine(String summary) {
     var [ident, matches] = summary.split(":");
-    //TODO - parse these properly
-    return Game(2, [(red: 1, green: 2, blue: 0)]);
+
+    var id_match = RegExp(r'Game (\d*)').matchAsPrefix(ident);
+    if (id_match == null) throw Exception("Game ID not found");
+
+    var match_descriptions = matches.split(";").map((e) => e.trim());
+
+    return Game(
+        int.parse(id_match[1]!), match_descriptions.map(_constructRecord));
   }
 
   bool meetsConstraint(({int red, int green, int blue}) constraint) {
     return true;
   }
+
+  static ({int red, int green, int blue}) _constructRecord(String match) {
+    //find red
+    var red_value = RegExp(r'.*?(\d+) red.*').firstMatch(match);
+    //find blue
+    var blue_value = RegExp(r'.*?(\d+) blue.*').firstMatch(match);
+    //find green
+    var green_value = RegExp(r'.*?(\d+) green.*').firstMatch(match);
+
+    return (
+      red: int.tryParse(red_value?[1] ?? '') ?? 0,
+      blue: int.tryParse(blue_value?[1] ?? '') ?? 0,
+      green: int.tryParse(green_value?[1] ?? '') ?? 0
+    );
+  }
 }
 
 void main() {
+  group('core', () {
+    var game = Game.fromLine(
+        'Game 30: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red');
+    test('factory finds id correctly', () {
+      expect(game.id).toEqual(30);
+    });
+    test('factory finds matches correctly', () {
+      var expected = [
+        (green: 8, blue: 6, red: 20),
+        (blue: 5, red: 4, green: 13),
+        (green: 5, red: 1, blue: 0)
+      ];
+
+      expect(game.matches).toEqual(expected);
+    });
+  });
   group('Part 1', () {
     /* 
       Conditions:
@@ -34,10 +71,6 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
       """;
-
-      for (var line in example_lines.split("\n")) {
-        print(line.trim());
-      }
     });
   });
 }
