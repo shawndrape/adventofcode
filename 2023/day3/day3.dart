3a20ce2a3f2cd8ec5c9bed5924688b253a8409a6
Shawn Drape <shawn@dra.pe>
2023-12-05 09:58:46 -0500

    day3 pullNumber

--- 2023/day3/day3.dart
+++ 2023/day3/day3.dart
@@ -22,20 +22,69 @@ List<List<String>> traversable_schematic(String schematic) {
   return result;
 }
 
+enum Node {
+  ALPHA,
+  SYMBOL,
+  PERIOD,
+  DIGIT,
+  UNKNOWN,
+}
+
 ///based on https://www.rapidtables.com/code/text/ascii-table.html
-bool isSymbol(String char) {
+Node getNodeType(String char) {
   int charCode = char.codeUnitAt(0);
 
   switch (charCode) {
-    case < 33:
     case 46:
+      return Node.PERIOD;
     case >= 48 && < 58:
+      return Node.DIGIT;
     case >= 65 && < 91:
     case >= 97 && < 123:
-      return false;
+      return Node.ALPHA;
     default:
-      return true;
+      return Node.SYMBOL;
   }
+}
+
+Set<int> extract_numbers(List<List<String>> schem, ({int x, int y}) point) {
+  var result = <int>{};
+
+  var min_x = 0;
+  var min_y = 0;
+  var max_y = schem.length - 1;
+  var max_x = schem[0].length - 1;
+
+  //starting from point's top-left (-1, -1), scan for digits
+  for (var x = -1; x <= 1; x++) {
+    for (var y = -1; y <= 1; y++) {
+      if (x == 0 && y == 0) continue;
+      if (x < min_x || x > max_x) continue;
+      if (y < min_y || y > max_y) continue;
+      var curr_point = (x: point.x + x, y: point.y + y);
+      if (getNodeType(schem[curr_point.y][curr_point.x]) != Node.DIGIT)
+        continue;
+
+      int found_number = pullNumber(schem[curr_point.y], curr_point.x);
+      result.add(found_number);
+    }
+  }
+  return result;
+}
+
+///assumption: cursor already confirmed to be a digit
+///found a number! Traverse X-axis in both directions until no longer on a digit
+int pullNumber(List<String> line, int cursor) {
+  var left_bound = -1, right_bound = 1;
+  try {
+    while (getNodeType(line[cursor + left_bound]) == Node.DIGIT) left_bound--;
+  } on RangeError {}
+  try {
+    while (getNodeType(line[cursor + right_bound]) == Node.DIGIT) right_bound++;
+  } on RangeError {}
+  int found_number = int.parse(
+      line.sublist(cursor + left_bound + 1, cursor + right_bound).join());
+  return found_number;
 }
 
 Iterable<int> analyze_schematic(String schematic) {
@@ -46,15 +95,25 @@ void main() {
   group('part 1', () {
     test('test isSymbol', () {
       var sets = [
-        (',', true),
-        ('a', false),
-        ('.', false),
-        ('9', false),
-        (r'$', true),
+        (',', Node.SYMBOL),
+        ('a', Node.ALPHA),
+        ('.', Node.PERIOD),
+        ('9', Node.DIGIT),
+        (r'$', Node.SYMBOL),
       ];
       for (var i in sets) {
-        expect(isSymbol(i.$1)).toEqual(i.$2);
+        expect(getNodeType(i.$1)).toEqual(i.$2);
       }
+    });
+    test('test pullNumber', () {
+      var testInput = "...1234...".split("");
+      expect(pullNumber(testInput, 5)).toEqual(1234);
+
+      var test2 = "9876.....".split("");
+      expect(pullNumber(test2, 1)).toEqual(9876);
+
+      var test3 = "....*6789".split("");
+      expect(pullNumber(test3, 5)).toEqual(6789);
     });
     test('example', () {
       var extracted_schematic_numbers = analyze_schematic(example_schematic);
