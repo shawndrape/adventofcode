a0711ed83454831a170cc1b8d8bd7f96bbe5b0ce
Shawn Drape <shawn@dra.pe>
2023-12-05 16:04:44 -0500

    day4 part 2 success

--- 2023/day4/day4.dart
+++ 2023/day4/day4.dart
@@ -36,6 +36,32 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""";
   return (game: game_id, matches: matches, score: pointsForGame(matches));
 }
 
+class Card {
+  int id;
+  Set<int> winning_numbers;
+  List<int> chosen_numbers;
+
+  Card(this.id, this.winning_numbers, this.chosen_numbers);
+
+  factory Card.fromLine(String card_line) {
+    var [title, details] = card_line.split(":");
+    var game_id = int.parse(title.replaceFirst(RegExp(r"Card\s+"), ""));
+
+    var [winning_numbers, chosen_numbers] = details.split("|");
+    var winning_ints =
+        winning_numbers.trim().split(RegExp(r'\s+')).map(int.parse).toSet();
+    var chosen_ints =
+        chosen_numbers.trim().split(RegExp(r'\s+')).map(int.parse).toList();
+    return Card(game_id, winning_ints, chosen_ints);
+  }
+
+  int get matches =>
+      chosen_numbers.where((e) => winning_numbers.contains(e)).length;
+
+  List<int> get additional_cards =>
+      [for (var i = id + 1; i <= id + matches; i++) i];
+}
+
 void main() {
   group('part 1', () {
     test('example', () {
@@ -60,4 +86,50 @@ void main() {
       expect(card_winning_sum).toBe(22488);
     });
   });
+  group('part 2', () {
+    Stream<String> asyncExample() async* {
+      for (var line in example.split("\n")) {
+        yield line;
+      }
+    }
+
+    test('example', () async {
+      Map<int, int> cardCounts = await countCards(asyncExample());
+
+      var expected = {
+        1: 1,
+        2: 2,
+        3: 4,
+        4: 8,
+        5: 14,
+        6: 1,
+      };
+
+      expect(cardCounts).toEqual(expected);
+      expect(cardCounts.values.sum).toBe(30);
+    });
+
+    test('input file', () async {
+      var input = File('day4_input.txt');
+      final lines =
+          input.openRead().transform(utf8.decoder).transform(LineSplitter());
+
+      var cardCounts = await countCards(lines);
+
+      expect(cardCounts.values.sum).toBe(7013204);
+    });
+  });
+}
+
+Future<Map<int, int>> countCards(Stream<String> card_summary) async {
+  var cardCounts = <int, int>{};
+
+  await for (var line in card_summary) {
+    var card = Card.fromLine(line);
+    cardCounts[card.id] ??= 1;
+    var num_of_cards = cardCounts[card.id]!;
+    for (var won_card in card.additional_cards)
+      cardCounts[won_card] = (cardCounts[won_card] ?? 1) + num_of_cards;
+  }
+  return cardCounts;
 }
