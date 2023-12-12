8b9f311fe34c033ae94a5d802e15201a5b06056f
Shawn Drape <shawn@dra.pe>
2023-12-11 19:07:09 -0500

    day 7 part 1 success

--- 2023/day7/day7.dart
+++ 2023/day7/day7.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
 import 'package:spec/spec.dart';
 
 String card_ordering = "AKQJT98765432";
 
 enum HandType {
+  Unknown,
   HighCard,
   OnePair,
   TwoPair,
@@ -16,13 +20,39 @@ class Hand implements Comparable<Hand> {
   String cards;
   int bid;
 
-  Hand(this.cards, this.bid)
+  Hand(this.cards, [this.bid = 0])
       : assert(
             cards.split('').every((element) => card_ordering.contains(element)),
-            'Invalid Hand');
+            'Invalid Hand'),
+        assert(cards.length == 5, 'Not enough cards');
 
+  //general approach: make a map of the characters and determine based on
+  // counts and lengths in map
   HandType get type {
-    return HandType.HighCard;
+    var char_count = <String, int>{};
+    cards.split('').forEach((e) {
+      char_count[e] = (char_count[e] ?? 0) + 1;
+    });
+    var combinations = char_count.values.toList();
+    combinations.sort();
+    switch (combinations.reversed.toList()) {
+      case [5]:
+        return HandType.FiveKind;
+      case [4, 1]:
+        return HandType.FourKind;
+      case [3, 2]:
+        return HandType.FullHouse;
+      case [3, 1, 1]:
+        return HandType.ThreeKind;
+      case [2, 2, 1]:
+        return HandType.TwoPair;
+      case [2, 1, 1, 1]:
+        return HandType.OnePair;
+      case [1, 1, 1, 1, 1]:
+        return HandType.HighCard;
+      default:
+        return HandType.Unknown;
+    }
   }
 
   @override
@@ -48,13 +78,60 @@ class Hand implements Comparable<Hand> {
 void main() {
   group('core Hand', () {
     test('compareTo higher', () {
-      expect(Hand("AJ", 0).compareTo(Hand("A9", 0))).lessThan(0);
+      expect(Hand("AJ222").compareTo(Hand("A9222"))).lessThan(0);
     });
     test('compareTo lower', () {
-      expect(Hand("A9", 0).compareTo(Hand("AJ", 0))).greaterThan(0);
+      expect(Hand("A9222").compareTo(Hand("AJ222"))).greaterThan(0);
     });
     test('compareTo equal', () {
-      expect(Hand("AK", 0).compareTo(Hand("AK", 0))).toBe(0);
+      expect(Hand("AK222").compareTo(Hand("AK222"))).toBe(0);
+    });
+    [
+      ("AAAAA", HandType.FiveKind),
+      ("KKKJJ", HandType.FullHouse),
+      ("98877", HandType.TwoPair),
+      ("98765", HandType.HighCard),
+      ("AKKKK", HandType.FourKind),
+      ("AKKKT", HandType.ThreeKind),
+      ("8K768", HandType.OnePair),
+    ].forEach((e) {
+      test('hand ${e.$1} has type ${e.$2}', () {
+        expect(Hand(e.$1).type).toBe(e.$2);
+      });
+    });
+  });
+  group('part 1', () {
+    var input = """32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483""";
+    test('example', () {
+      var hands = input.split('\n').map((e) {
+        var [cards, bid] = e.split(" ");
+        return Hand(cards, int.parse(bid));
+      }).toList();
+
+      hands.sort();
+      var winnings = hands.reversed.foldIndexed(0,
+          (index, previous, element) => ((index + 1) * element.bid) + previous);
+
+      expect(winnings).toEqual(6440);
+    });
+    test('input file', () async {
+      var file = File('day7_input.txt');
+      var lines = await file.readAsLines();
+
+      var hands = lines.map((e) {
+        var [cards, bid] = e.split(" ");
+        return Hand(cards, int.parse(bid));
+      }).toList();
+
+      hands.sort();
+      var winnings = hands.reversed.foldIndexed(0,
+          (index, previous, element) => ((index + 1) * element.bid) + previous);
+
+      expect(winnings).toEqual(252052080);
     });
   });
 }
