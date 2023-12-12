0a0027c7ef79c6c134ce7e417ea510e26ac269fa
Shawn Drape <shawn@dra.pe>
2023-12-11 19:40:02 -0500

    day 7 part 2 pause

--- 2023/day7/day7.dart
+++ 2023/day7/day7.dart
@@ -4,6 +4,7 @@ import 'package:collection/collection.dart';
 import 'package:spec/spec.dart';
 
 String card_ordering = "AKQJT98765432";
+String card_ordering_with_jokers = "AKQT98765432J";
 
 enum HandType {
   Unknown,
@@ -19,16 +20,27 @@ enum HandType {
 class Hand implements Comparable<Hand> {
   String cards;
   int bid;
+  bool using_jokers;
 
-  Hand(this.cards, [this.bid = 0])
+  Hand(this.cards, [this.bid = 0, this.using_jokers = false])
       : assert(
             cards.split('').every((element) => card_ordering.contains(element)),
             'Invalid Hand'),
         assert(cards.length == 5, 'Not enough cards');
 
+  bool get hand_has_jokers => using_jokers && cards.contains("J");
+
+  int get num_of_jokers => cards.split("").where((e) => e == "J").length;
+
   //general approach: make a map of the characters and determine based on
   // counts and lengths in map
   HandType get type {
+    return !hand_has_jokers
+        ? _handAnalyzerWithoutJokers()
+        : _handAnalyzerWithJokers();
+  }
+
+  HandType _handAnalyzerWithoutJokers() {
     var char_count = <String, int>{};
     cards.split('').forEach((e) {
       char_count[e] = (char_count[e] ?? 0) + 1;
@@ -55,14 +67,50 @@ class Hand implements Comparable<Hand> {
     }
   }
 
+  HandType _handAnalyzerWithJokers() {
+    var char_count = <String, int>{};
+    cards.split('').forEach((e) {
+      char_count[e] = (char_count[e] ?? 0) + 1;
+    });
+    var current_num_of_jokers = char_count.remove("J") ?? 0;
+    //TODO - pattern matching can't mix literals and variables, so go through
+    //the char_count and add the jokers to the highest value remaining
+    // Edge case: Oops all jokers
+    var combinations = char_count.values.toList();
+    combinations.sort();
+    switch (combinations.reversed.toList()) {
+      case [5]:
+        return HandType.FiveKind;
+      case [4, 1]:
+        return HandType.FourKind;
+      case [3, 2]:
+        return HandType.FullHouse;
+      case [3, 1, 1]:
+        return HandType.ThreeKind;
+      case [2, 2, 1]:
+        return HandType.TwoPair;
+      case [2, 1, 1, 1]:
+        return HandType.OnePair;
+      case [1, 1, 1, 1, 1]:
+        return HandType.HighCard;
+      default:
+        return HandType.Unknown;
+    }
+  }
+
   @override
   int compareTo(Hand other) {
+    //reject mismatched joker rules
+    assert(using_jokers == other.using_jokers,
+        'Can not compare joker and non-joker rules');
     //same hand
     if (this.cards == other.cards) return 0;
     //rank hand type
     if (this.type != other.type) return other.type.index - this.type.index;
     //compare card ordering
-    var ordering_reversed = card_ordering.split('').reversed.join();
+    var ordering_to_use =
+        using_jokers ? card_ordering_with_jokers : card_ordering;
+    var ordering_reversed = ordering_to_use.split('').reversed.join();
     var index = 0;
     try {
       while (this.cards[index] == other.cards[index]) index++;
