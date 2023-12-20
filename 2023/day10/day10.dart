f4daea8f3c41b310f45c397fa74875c01628b938
Shawn Drape <shawn@dra.pe>
2023-12-19 23:19:28 -0500

    day10 part 2 attempt 1
    The idea is to expand the grid so we can also track which side of the pipe wall is the "inner" wall vs. the "outer" wall. Then we find all nodes within inner walls ignoring node type

--- 2023/day10/day10.dart
+++ 2023/day10/day10.dart
@@ -100,6 +100,77 @@ Point<int> findStartingPipe(List<String> grid) {
   return (stepCount, currentA);
 }
 
+//expected answer: 4
+var part2example1 = """
+..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........""";
+
+List<String> expandGrid(List<String> grid) {
+  var result = <String>[];
+  int? gridLength;
+  for (var line in grid) {
+    result.add(line.split("").join("G"));
+    gridLength ??= result[0].length;
+    result.add(List<String>.generate(gridLength, (_) => "G", growable: false)
+        .join(""));
+  }
+  result.removeLast();
+  return result;
+}
+
+Set<Point<int>> connectedSegmentsExpanded(String char) => switch (char) {
+      "F" => {Point(2, 0), Point(1, 0), Point(0, 1), Point(0, 2)},
+      "L" => {Point(0, -2), Point(0, -1), Point(1, 0), Point(2, 0)},
+      "J" => {Point(0, -2), Point(0, -1), Point(-1, 0), Point(-2, 0)},
+      "7" => {Point(-2, 0), Point(-1, 0), Point(0, 1), Point(0, 2)},
+      "-" => {Point(-2, 0), Point(-1, 0), Point(1, 0), Point(2, 0)},
+      "|" => {Point(0, -2), Point(0, -1), Point(0, 1), Point(0, 2)},
+      "." => {},
+      "G" => throw Exception("evaluating a gap"),
+      "I" => throw Exception("evaluating inner wall"),
+      "O" => throw Exception("Evaluating outer wall"),
+      _ => throw Exception("Invalid symbol")
+    };
+
+Set<Point<int>> findInitialDirectionsExpanded(
+    List<String> grid, Point<int> start) {
+  assert(grid[start.y][start.x] == "S");
+  var startingPoints = <Point<int>>{};
+  var minX = 0;
+  var minY = 0;
+  var maxY = grid.length - 1;
+  var maxX = grid[0].length - 1;
+
+  //starting from point's top-left (-2, -2), scan for pipes
+  for (var y = -2; y <= 2; y += 2) {
+    for (var x = -2; x <= 2; x += 2) {
+      if (x == 0 && y == 0) continue;
+      var currX = start.x + x;
+      var currY = start.y + y;
+      if (currX < minX || currX > maxX) continue;
+      if (currY < minY || currY > maxY) continue;
+      var directions = connectedSegmentsExpanded(grid[currY][currX]);
+      if (directions.isEmpty) continue;
+      var contributingPoints = directions.map((e) => Point(currX, currY) + e);
+      if (contributingPoints.any((element) => element == start)) {
+        startingPoints.addAll(contributingPoints);
+        startingPoints.add(Point(currX, currY));
+      }
+    }
+  }
+
+  startingPoints.remove(start);
+  assert(startingPoints.length == 8);
+  return startingPoints;
+}
+
 void main() {
   group('utils', () {
     test('find start', () {
@@ -140,6 +211,48 @@ void main() {
       var (steps, _) = findTheMidpoint(grid);
 
       expect(steps).toEqual(6701);
+    });
+  });
+  group('part 2', () {
+    test('expand grid', () {
+      var exampleGrid = """
+.....
+.S-7.
+.|.|.
+.L-J.
+.....""";
+
+      var expectedExpansion = """
+.G.G.G.G.
+GGGGGGGGG
+.GSG-G7G.
+GGGGGGGGG
+.G|G.G|G.
+GGGGGGGGG
+.GLG-GJG.
+GGGGGGGGG
+.G.G.G.G.""";
+      var expandedGrid = expandGrid(exampleGrid.split("\n"));
+      expect(expandedGrid.join("\n")).toEqual(expectedExpansion);
+    });
+    test(solo: true, 'identify expanded starts', () {
+      var input = """
+...
+.S-
+.|.""";
+      var expandedGrid = expandGrid(input.split("\n"));
+      var start = findStartingPipe(expandedGrid);
+
+      expect(findInitialDirectionsExpanded(expandedGrid, start)).toEqual({
+        Point(3, 2),
+        Point(5, 2),
+        Point(6, 2),
+        Point(4, 2),
+        Point(2, 3),
+        Point(2, 5),
+        Point(2, 6),
+        Point(2, 4)
+      });
     });
   });
 }
