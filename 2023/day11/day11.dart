73a1b1cbdaa272cf90e173da7004532e840ffbe6
Shawn Drape <shawn@dra.pe>
2023-12-21 22:49:26 -0500

    refactor day11
    This was really an interesting exercise in syntactic sugar

--- 2023/day11/day11.dart
+++ 2023/day11/day11.dart
@@ -16,34 +16,6 @@ var example = """
 .......#..
 #...#.....""";
 
-List<String> expandUniverse(List<String> grid) {
-  var result = <String>[];
-  for (var line in grid) {
-    if (line.contains("#")) {
-      result.add(line);
-    } else {
-      result.addAll([line, line]);
-    }
-  }
-  var emptyGalaxyColumns = [
-    for (var x = 0; x < result[0].length; x++)
-      if (!result.any((element) => element[x] == "#")) x
-  ];
-  var finalValue = result.map((e) {
-    var stringArray = e.split('');
-    for (int j = 0; j < emptyGalaxyColumns.length; j++) {
-      var i = emptyGalaxyColumns[j];
-      stringArray.insert(i + j, '.');
-    }
-    return stringArray.join();
-  }).toList();
-  return finalValue;
-}
-
-extension GalaxyMapping on Point<int> {
-  int numSteps() => x.abs() + y.abs();
-}
-
 extension on int {
   bool between(int a, int b) {
     if (a == b) return false; //can not be between single value
@@ -55,35 +27,7 @@ extension on int {
   }
 }
 
-///Collect the points of galaxies, and put them on a set of pairs (ignores duplicates)
-///Then subtract the points in each pair and record the steps that point represents
-Map<(Point<int>, Point<int>), int> pathsWithSteps(List<String> input) {
-  var grid = expandUniverse(input);
-
-  var knownGalaxies = [
-    for (int yAxis = 0; yAxis < grid.length; yAxis++)
-      for (int xAxis = 0; xAxis < grid[0].length; xAxis++)
-        if (grid[yAxis][xAxis] == "#") Point(xAxis, yAxis)
-  ];
-  var galaxyPairs = <(Point<int>, Point<int>)>{
-    for (var x = 0; x < knownGalaxies.length; x++)
-      for (var y = x + 1; y < knownGalaxies.length; y++)
-        (knownGalaxies[x], knownGalaxies[y])
-  };
-
-  // var steps = galaxyPairs.map((e) {
-  //   var (pointA, pointB) = e;
-  //   var difference = pointA - pointB;
-  //   return difference.numSteps();
-  // });
-  var steps = {
-    for (var pair in galaxyPairs) pair: (pair.$1 - pair.$2).numSteps()
-  };
-  return steps;
-}
-
-Map<(Point<int>, Point<int>), int> pathsWithStepsOfExpandedUniverse(
-    List<String> grid,
+Map<(Point<int>, Point<int>), int> pathsWithSteps(List<String> grid,
     [int expansionFactor = 2]) {
   var emptyGalaxyColumns = [
     for (var x = 0; x < grid[0].length; x++)
@@ -112,9 +56,10 @@ Map<(Point<int>, Point<int>), int> pathsWithStepsOfExpandedUniverse(
     var numOfEmptyRowsOnPath =
         emptyGalaxyRows.where((e) => e.between(pointA.y, pointB.y)).length;
 
-    var difference = pointA - pointB;
+    Point<int> path = pointA - pointB;
     //we -1 the expansion factor to reflect that one "step" is taken in numSteps
-    return difference.numSteps() +
+    return path.x.abs() +
+        path.y.abs() +
         (numOfEmptyColumnsOnPath + numOfEmptyRowsOnPath) *
             (expansionFactor - 1);
   }
@@ -127,28 +72,10 @@ Map<(Point<int>, Point<int>), int> pathsWithStepsOfExpandedUniverse(
 }
 
 void main() {
-  group('core utils', () {
-    test('expandUniverse', () {
-      var expected = """
-....#........
-.........#...
-#............
-.............
-.............
-........#....
-.#...........
-............#
-.............
-.............
-.........#...
-#....#.......""";
-
-      expect(expandUniverse(example.split("\n"))).toEqual(expected.split("\n"));
-    });
-  });
+  sum(Map<Object, int> map) => map.values.sum;
   group('part 1', () {
     test('example', () {
-      expect(pathsWithSteps(example.split("\n")).values.sum).toEqual(374);
+      expect(sum(pathsWithSteps(example.split("\n")))).toEqual(374);
     });
     test('input file', () async {
       var file = File('day11_input.txt');
@@ -156,7 +83,7 @@ void main() {
       var grid = blob.split("\n");
 
       var actual = pathsWithSteps(grid);
-      expect(actual.values.sum).toEqual(9445168);
+      expect(sum(actual)).toEqual(9445168);
     });
   });
   group('part 2', () {
@@ -164,34 +91,32 @@ void main() {
     //so rather than simulate the board, track the axis points where gaps occur
     //and add steps accordingly.
     test('example via both mechanisms', () {
-      var actualFactor1 = pathsWithStepsOfExpandedUniverse(example.split("\n"));
+      var actualFactor1 = pathsWithSteps(example.split("\n"));
 
-      expect(actualFactor1.values.sum)
-          .toEqual(pathsWithSteps(example.split("\n")).values.sum);
+      expect(sum(actualFactor1))
+          .toEqual(sum(pathsWithSteps(example.split("\n"))));
     });
     test('input file for part 1 equivalency', () async {
       var file = File('day11_input.txt');
       var blob = await file.readAsString();
       var grid = blob.split("\n");
 
-      var actual = pathsWithStepsOfExpandedUniverse(grid);
-      expect(actual.values.sum).toEqual(9445168);
+      var actual = pathsWithSteps(grid);
+      expect(sum(actual)).toEqual(9445168);
     });
     test('example w/ more expansions', () {
       var grid = example.split('\n');
 
-      expect(pathsWithStepsOfExpandedUniverse(grid, 10).values.sum)
-          .toEqual((1030));
-      expect(pathsWithStepsOfExpandedUniverse(grid, 100).values.sum)
-          .toEqual((8410));
+      expect(sum(pathsWithSteps(grid, 10))).toEqual((1030));
+      expect(sum(pathsWithSteps(grid, 100))).toEqual((8410));
     });
     test('input file', () async {
       var file = File('day11_input.txt');
       var blob = await file.readAsString();
       var grid = blob.split("\n");
 
-      var actual = pathsWithStepsOfExpandedUniverse(grid, 1000000);
-      expect(actual.values.sum).toEqual(742305960572);
+      var actual = pathsWithSteps(grid, 1000000);
+      expect(sum(actual)).toEqual(742305960572);
     });
   });
 }
