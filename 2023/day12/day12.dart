74d897a122a0a95663ef5339ba778f2fac102779
Shawn Drape <shawn@dra.pe>
2023-12-25 14:31:38 -0500

    implemented clear grouping cases

--- 2023/day12/day12.dart
+++ 2023/day12/day12.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show max;
+
 import 'package:collection/collection.dart';
 import 'package:spec/spec.dart';
 
@@ -11,6 +13,7 @@ var exampleInput = """
 
 extension on String {
   String trimOperationalNodes() => replaceAll(RegExp(r'^\.+|\.+$'), '');
+  String trimBrokenNodes() => replaceAll(RegExp(r'^#+|#+$'), '');
 }
 
 bool isValidGrouping(String field, List<int> groups) {
@@ -35,24 +38,49 @@ int countPossibleConfigurations(String field, List<int> brokenGroups) {
 
   //there is a viable strategy here to remove fully confirmed groups
   //based on their large size and the recursively process the rest of the field
-
+  int totalPossibleConfigurationCount = 0;
   var viableGroupsPattern = RegExp(r'([\?#]+)');
   print('processing $field as $fieldTrimmed');
   var matches = viableGroupsPattern.allMatches(fieldTrimmed);
+
+  // for (var match in matches) {
+  //   print("Found pattern from ${match.start} to ${match.end}");
+  //   print("Contains group: ${match[1]}");
+  // }
+
   if (matches.length == brokenGroups.length) {
     print('confirmed contiguous groups');
+    for (var (i, match) in matches.indexed) {
+      var expectedLength = brokenGroups[i];
+      if (expectedLength == match[1]?.length) {
+        continue; //effectively wiggle room zero
+      } else {
+        // remove known nodes
+        String matchedGroup = match[1]!;
+        print(
+            'processing $matchedGroup to find group of length $expectedLength');
+        var remainingUnknowns = matchedGroup.trimBrokenNodes();
+        var remainingExpectedLength =
+            expectedLength - (matchedGroup.length - remainingUnknowns.length);
+        var allUnknowns = remainingUnknowns.split("").every((e) => e == '?');
+        if (allUnknowns &&
+            remainingExpectedLength <= remainingUnknowns.length) {
+          var possibleConfigsCount =
+              (remainingUnknowns.length - remainingExpectedLength + 1);
+          totalPossibleConfigurationCount += possibleConfigsCount;
+        }
+      }
+    }
   } else {
     print('fewer groups found than known. Some ? are functional');
+    return -1; //TODO - implement
   }
-  for (var match in matches) {
-    print("Found pattern from ${match.start} to ${match.end}");
-    print("Contains group: ${match[1]}");
-  }
-  return 0;
+  //if all groupings exactly match and the counter never incremented, return 1
+  return max(1, totalPossibleConfigurationCount);
 }
 
 void main() {
-  group(solo: true, 'utils', () {
+  group('utils', () {
     test('trim the known operational nodes on ends of field', () {
       expect("...#?#...".trimOperationalNodes()).toEqual("#?#");
       expect(countPossibleConfigurations(".???.###", [1, 1, 3])).toBe(1);
